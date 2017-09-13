ff2b48722d2f4b323f2a9ede01f4852a122f27f6
Max Walters <max.walters10@gmail.com>
2017-09-13 01:43:12 -0400

    Fixed basicEnemy.cs
    Added comments and fixed Player error through debugging

--- assets/scripts/basicEnemy.cs
+++ assets/scripts/basicEnemy.cs
@@ -5,38 +5,54 @@ public class BasicEnemy : MovingObject {
 
   public int playerDamage;
   public int enemyHealth;
+  public GameObject player;
 
+  private Vector3 transformTemp;
   private Animator animator;
   private Transform target;
 
-  protected override void Start ()
-  {
+  protected override void Start () {
+	transformTemp = transform.position;
     animator = GetComponent<Animator>();
     target = GameObject.FindGameObjectWithTag ("Player").transform;
     base.Start();
   }
 
-  protected override void AttemptMove <T> (int xDir, int yDir)
-  {
+ void Update() {
+	
+
+	transformTemp = transform.position;
+	Vector3 targetPosition = target.transform.position;
+
+ }
+
+  protected override void AttemptMove <T> (int xDir, int yDir) {
     base.AttemptMove <T> (xDir, yDir);
   }
 
-  public void MoveEnemy()
-  {
+  public void MoveEnemy()  {
+		
     int xDir = 0;
     int yDir = 0;
 
-    if (Mathf.Abs (target.position.x = transform.position.x) < float.Epsilon)
+		//Compare if objects are on the same x-axis
+		//If so, move on the y-axis to the player
+		//If not, move on x-axis to the player
+	if (target.position.x == transformTemp.x)
       yDir = target.position.y > transform.position.y ? 1 : -1;
     else
       xDir = target.position.x > transform.position.x ? 1 : -1;
 
-    AttemptMove <Player> (xDir, yDir)
+        AttemptMove<PlayerController>(xDir, yDir);
   }
 
-  protected override void OnCantMove <T> (T component)
-  {
-    Player hitPlayer = component as Player;
+    /**
+     * LoseHealth is needed in PlayerController
+     * If enemy can't move, check to see if colliding with player,
+     * If colliding with player, deal damage to player
+     * */
+    protected override void OnCantMove <T> (T component) {
+    PlayerController hitPlayer = component as PlayerController;
 
     hitPlayer.LoseHealth(playerDamage);
   }
