557b33d6aa83d2d094edd6cb1c1348a369b10e04
Max Walters <max.walters10@gmail.com>
2017-09-25 10:25:00 -0400

    working on enemy movement

--- assets/scripts/basicEnemy.cs
+++ assets/scripts/basicEnemy.cs
@@ -1,60 +1,35 @@
 using UnityEngine;
 using System.Collections;
 
-public class BasicEnemy : MovingObject {
+public class BasicEnemy : MonoBehaviour {
 
-  public int playerDamage;
-  public int enemyHealth;
-  public GameObject player;
+    public int playerDamage;
+    public int enemyHealth;
+    public int moveSpeed;
+    public PlayerController player;
 
-  private Vector3 transformTemp;
-  private Animator animator;
-  private Transform target;
+    private Vector2 transformTemp;
+    private Transform target;
 
-  protected override void Start () {
-	transformTemp = transform.position;
-    animator = GetComponent<Animator>();
-    target = GameObject.FindGameObjectWithTag ("Player").transform;
-    base.Start();
-  }
+    protected override void Start () {
 
- void Update() {
-	
+        transformTemp = transform.position;
+        target = GameObject.FindGameObjectWithTag ("Player").transform;
+        base.Start();
+    }
 
-	transformTemp = transform.position;
-	Vector3 targetPosition = target.transform.position;
+    void Update() {
 
- }
+        transformTemp = transform.position;
+        Vector2 targetPosition = target.transform.position;
+    }
 
-  protected override void AttemptMove <T> (int xDir, int yDir) {
-    base.AttemptMove <T> (xDir, yDir);
-  }
+    public void MoveEnemy()  {
 
-  public void MoveEnemy()  {
-		
-    int xDir = 0;
-    int yDir = 0;
+        if(player.position.x == ) {//enter enemy x position here
 
-		//Compare if objects are on the same x-axis
-		//If so, move on the y-axis to the player
-		//If not, move on x-axis to the player
-	if (target.position.x == transformTemp.x)
-      yDir = target.position.y > transform.position.y ? 1 : -1;
-    else
-      xDir = target.position.x > transform.position.x ? 1 : -1;
+        }
+    }
 
-
-        AttemptMove<PlayerController>(xDir, yDir);
-  }
-
-    /**
-     * LoseHealth is needed in PlayerController
-     * If enemy can't move, check to see if colliding with player,
-     * If colliding with player, deal damage to player
-     * */
-    protected override void OnCantMove <T> (T component) {
-    PlayerController hitPlayer = component as PlayerController;
-
-    hitPlayer.LoseHealth(playerDamage);
-  }
+    
 }
