52120eff62d859d212a8c13c5893e8a9320874ed
CJSheehan15 <sheehac5@miamioh.edu>
2017-11-10 19:41:09 -0500

    camera adjusted and environment stuff
    FoV adjusted by 25%
    
    Environment stuff added
    
    sorting layers adjusted for fog

---
+++ Assets/Scripts/CameraViewControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewControl : MonoBehaviour {
+
+	//update 
+    //if player stays collides with tag of other
+    //read others desiredFoV 
+    //camera Fov = desiredFoV lerp?
+    //once desiredFoV = camFoV stop lerping 
+
+}
