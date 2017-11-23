c4ee5d31b40d1aa63a68e7bc7b921095c82fe3a5
CJSheehan15 <sheehac5@miamioh.edu>
2017-11-23 01:42:43 -0500

    the new stuff and looots of it

--- Assets/Scripts/ImageEffectsZone.cs
+++ Assets/Scripts/ImageEffectsZone.cs
@@ -5,16 +5,38 @@ using UnityStandardAssets.ImageEffects;
 
 public class ImageEffectsZone : MonoBehaviour {
 
-    void OnTriggerEnter2D()
+    public Transform player;
+
+    public GameObject playerCamera;
+
+   /* void OnTriggerEnter2D()
     {
         
-        Camera.main.GetComponent<Bloom>().enabled = true;
+        //Camera.main.GetComponent<Bloom>().enabled = true;
 
     }
     void OnTriggerExit2D()
     {
 
-        Camera.main.GetComponent<Bloom>().enabled = false;
+       // Camera.main.GetComponent<Bloom>().enabled = false;
 
+    }*/
+
+    void Update()
+    {
+        if (player)
+        {
+            float dist = Vector3.Distance(player.position, transform.position);
+            //print("Distance to player: " + dist);
+
+            if (dist <= 15 && dist >= 1)
+            {
+                playerCamera.GetComponent<Bloom>().bloomIntensity = 2 / (dist);
+            }
+            if (dist > 15)
+            {
+                playerCamera.GetComponent<Bloom>().bloomIntensity = 0;
+            }
+        }
     }
 }
