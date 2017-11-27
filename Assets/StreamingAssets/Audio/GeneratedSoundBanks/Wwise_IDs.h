0470f9ba2034c4565e3dee10ef77c91234d0f20c
sjmpson <sjmpson11@gmail.com>
2017-11-27 04:00:55 -0500

    AUDIO - Wwise Again
    I hope this version does not get lost it is exhausting redoing the pipeline thx

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,36 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
