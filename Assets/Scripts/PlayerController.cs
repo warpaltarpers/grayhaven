78d9551fbf5bc42259a4ad29121c4cb2966f0ef6
sportsad <sportsad@miamioh.edu>
2017-12-03 21:11:01 -0500

    More animations changes to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -63,9 +63,10 @@ public class PlayerController : MonoBehaviour
 
 	void FixedUpdate ()
 	{
-		// This constantly checks whether or not the player is touching the ground
-		//grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
-		//anim.SetBool ("Ground", grounded);
+        // This constantly checks whether or not the player is touching the ground
+        //grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+
+		anim.SetBool ("Ground", isGrounded);
 
 
         //track vertical velocity
