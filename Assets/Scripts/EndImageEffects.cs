ce1829c81a3c7d526fa0b7317242e393439d8aaf
CJSheehan15 <sheehac5@miamioh.edu>
2017-12-04 19:13:44 -0500

    UI, Boss, and Tweaking
    Fonts for buttons changed.
    Buttons and button highlighting changed.
    Title Screen Changed,
    o and p for health cheat commented out.
    Boss Trigger
    Boss animation ,
    and level tweaks to sorting layers here and there as well as placement
    of platforms tweaked

--- Assets/Scripts/EndImageEffects.cs
+++ Assets/Scripts/EndImageEffects.cs
@@ -2,71 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.ImageEffects;
+using UnityEngine.SceneManagement;
 
 public class EndImageEffects : MonoBehaviour {
 
+    public GameObject playerCamera;
 
-    public bool EndEffectsTriggered;
-    public bool End;
+    public GameObject[] TheEndLine1;
+    /*public GameObject TheEndLine2;
+    public GameObject TheEndLine3;
+    public GameObject TheEndLine4;
+    public GameObject TheEndLine5;
+    public GameObject TheEndLine6;*/
 
-    private float t = 0; // lerp control variable
-    public float duration = 15; // duration in seconds
-    public float startThresholdR;
-    public float startThresholdG;
-    public float startThresholdB;
 
-    // Use this for initialization
+    public Animator Anim;
+
+    bool Triggered;
+
+    static float t = 0.0f;
+    float currentFoV;
+
     void Start()
     {
-        startThresholdR = 225;
-        startThresholdR = 192;
-        startThresholdR = 174;
-
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.transform.tag == "Player")
+        for (int i = 0; i < TheEndLine1.Length; i++)
         {
-            //EndEffectsTriggered = true;
-            //Camera.main.GetComponent<Bloom>().enabled = true;
-            //Camera.main.GetComponent<SunShafts>().enabled = true;
-
-
+            TheEndLine1[i].SetActive(false);
+            
         }
     }
     void OnTriggerEnter2D()
     {
-        //if (other.transform.tag == "Player")
-        //{
-        // EndEffectsTriggered = true;
-        Camera.main.GetComponent<Bloom>().enabled = true;
-        Camera.main.GetComponent<SunShafts>().enabled = true;
+
+        
+
+        gameObject.GetComponent<Animator>().enabled = true;
+
+        StartCoroutine(ToTitleAgain());
+
+        // playerCamera.camera.
+        //Camera.main.fieldOfView = Mathf.Lerp(100, 110, t);
+        Triggered = true;
 
 
-        // }
+
 
     }
-    // Update is called once per frame
+
     void Update()
-    {
-
-
-
-        if (EndEffectsTriggered && !End)
+    { 
+        if (Triggered)
         {
-            End = true;
-            if (t < 1)
-            {
-                Camera.main.GetComponent<SunShafts>().sunThreshold.r = Mathf.Lerp(startThresholdR, 0, t);
-                Camera.main.GetComponent<SunShafts>().sunThreshold.g = Mathf.Lerp(startThresholdG, 0, t);
-                Camera.main.GetComponent<SunShafts>().sunThreshold.b = Mathf.Lerp(startThresholdB, 0, t);
-                // while t below the end limit...
-                // increment it at the desired rate every update:
-                t += Time.deltaTime / duration;
-            }
+            currentFoV = Camera.main.fieldOfView;
+            Camera.main.fieldOfView = Mathf.Lerp(currentFoV, 130, 2 * Time.deltaTime);
         }
     }
+    IEnumerator ToTitleAgain()
+    {
+        yield return new WaitForSeconds(2);
+        Anim.SetTrigger("bossTrigger");
+        yield return new WaitForSeconds(3);
+        //TheEndCanvas.SetActive(true);
+        for(int i = 0; i < TheEndLine1.Length; i++)
+        {
+            TheEndLine1[i].SetActive(true);
+            yield return new WaitForSeconds(3);
+        }
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+
 
 
 }
